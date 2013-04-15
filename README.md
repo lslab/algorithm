5f12e0a3fc4baafb69c8fb7cf8b035e992e0ba2b
lslab <lslab@sina.com>
2013-04-15 20:11:30 +0800

    eight permutations

--- README.md
+++
@@ -1,4 +0,0 @@
-algorithm
-=========
-
-practice algorithm
--- src/lslab/algorithm/sort/Sortable.java
+++
@@ -1,7 +0,0 @@
-package lslab.algorithm.sort;
-
-public interface Sortable<T extends Comparable<T>> {
-	public void sort(T[] a);
-	public void sort(int[] a);
-	public void setPrintFlag(boolean isPrint);
-}
